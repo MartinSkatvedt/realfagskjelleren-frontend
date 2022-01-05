7256d64325ffe99dd721f461a611139b018cf924
Martinskatvedt <martinskatvedt@gmail.com>
2022-01-05 17:26:45 +0100

    update config

--- src/oidc/configuration.tsx
+++ src/oidc/configuration.tsx
@@ -1,12 +1,12 @@
 const configuration = {
   client_id: process.env.REACT_APP_OIDC_ID,
-  redirect_uri: process.env.REACT_APP_CLIENT_HOST + "/authentication/callback",
+  redirect_uri: process.env.REACT_APP_VERCEL_URL + "/authentication/callback",
   response_type: "code",
-  post_logout_redirect_uri: process.env.REACT_APP_CLIENT_HOST + "/Dashboard",
+  post_logout_redirect_uri: process.env.REACT_APP_VERCEL_URL + "/Dashboard",
   scope: "openid profile",
   authority: process.env.REACT_APP_OIDC_AUTHORITY,
   silent_redirect_uri:
-    process.env.REACT_APP_CLIENT_HOST + "/authentication/silent_callback",
+    process.env.REACT_APP_VERCEL_URL + "/authentication/silent_callback",
   automaticSilentRenew: true,
   loadUserInfo: true,
 };
