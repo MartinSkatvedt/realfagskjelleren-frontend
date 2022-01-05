31169589828263fd7d6c0ab208ad9cfe6c9263e9
Martinskatvedt <martinskatvedt@gmail.com>
2022-01-05 17:24:18 +0100

    added host to env

--- src/oidc/configuration.tsx
+++ src/oidc/configuration.tsx
@@ -1,11 +1,12 @@
 const configuration = {
   client_id: process.env.REACT_APP_OIDC_ID,
-  redirect_uri: "http://localhost:3000/authentication/callback",
+  redirect_uri: process.env.REACT_APP_CLIENT_HOST + "/authentication/callback",
   response_type: "code",
-  post_logout_redirect_uri: "http://localhost:3000/Dashboard",
+  post_logout_redirect_uri: process.env.REACT_APP_CLIENT_HOST + "/Dashboard",
   scope: "openid profile",
   authority: process.env.REACT_APP_OIDC_AUTHORITY,
-  silent_redirect_uri: "http://localhost:3000/authentication/silent_callback",
+  silent_redirect_uri:
+    process.env.REACT_APP_CLIENT_HOST + "/authentication/silent_callback",
   automaticSilentRenew: true,
   loadUserInfo: true,
 };
